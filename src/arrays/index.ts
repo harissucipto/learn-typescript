338ccc4cd3cfa33cfa5f41bc6f026642e729fd08
haris sucipto <harisssucipto@gmail.com>
2021-11-20 20:57:04 +0700

    add arrays of types

---
+++ src/arrays/index.ts
@@ -0,0 +1,20 @@
+// Array
+let arrayOfNumber: number[] = [1, 2, 3];
+
+// ussage;
+arrayOfNumber = [1];
+arrayOfNumber = [1, 2, 3];
+
+// erro usage
+// arrayOfNumber = ['hello
+
+// Tuple
+// Tuple is array with fixed length
+let tuple: [number, number] = [0, 0];
+
+// Usage
+tuple = [1, 1];
+tuple = [2, 3];
+// tuple = [5] // Error: must be 2 items
+// tuple  = [5, 4, 3] // Erorr: must be 2 items
+// tuple = ["elite", 1337]; // Error: must be number
