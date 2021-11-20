9aafd0ed7a56160883530c9945b37edde00fd65f
haris sucipto <harisssucipto@gmail.com>
2021-11-20 21:14:58 +0700

    add type alias on object

---
+++ objects/index.ts
@@ -0,0 +1,10 @@
+type Point = { x: number; y: number };
+
+// using types alias
+let center: Point = { x: 0, y: 0 };
+
+let unit: Point = { x: 1, y: 1 };
+
+//adavantages of using type alias
+// 1. type alias can be used to create a type that is more readable
+// 2. type alias can be used to create a type that is more flexible
