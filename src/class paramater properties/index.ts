fecc7137963c8cb21fc3d2777b874f7807b7e273
haris sucipto <harisssucipto@gmail.com>
2021-11-21 23:21:22 +0700

    class paramater properties

---
+++ src/class paramater properties/index.ts
@@ -0,0 +1,7 @@
+// we can make variable in class parameter properties by defined in constructor
+class Person2 {
+  constructor(public name: string, public age: number) {
+    this.name = name;
+    this.age = age;
+  }
+}
