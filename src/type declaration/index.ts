0591ff5451d817c2e971a3b5df49b07a5943d923
haris sucipto <harisssucipto@gmail.com>
2021-11-21 21:23:25 +0700

    add async await

---
+++ src/type declaration/index.ts
@@ -0,0 +1,6 @@
+console.log("Logged in user:", process.env.USER);
+
+// if type not defined in envirotnment variable, then it will be undefined
+// declare const process: any;
+
+// or using declartion file by env.d.ts
